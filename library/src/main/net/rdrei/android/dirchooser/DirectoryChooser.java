48f5dc18127073b3f9c13459634aa83cd4b8f5af
Pascal Hartig <phartig@rdrei.net>
2013-01-09 23:39:22 +0100

    Library boilerplate

---
+++ library/src/main/net/rdrei/android/dirchooser/DirectoryChooser.java
@@ -0,0 +1,15 @@
+package net.rdrei.android.dirchooser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DirectoryChooser extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
