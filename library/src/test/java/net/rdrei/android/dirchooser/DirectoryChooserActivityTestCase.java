2e4f4803c782aaa9685ddb1d45543ddd7c3ea393
Pascal Hartig <passy@twitter.com>
2013-12-31 15:31:04 +0100

    fix(build): checkstyle enforcement

--- library/src/test/java/net/rdrei/android/dirchooser/DirectoryChooserActivityTestCase.java
+++ library/src/test/java/net/rdrei/android/dirchooser/DirectoryChooserActivityTestCase.java
@@ -11,28 +11,28 @@ import org.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
 public class DirectoryChooserActivityTestCase {
-	private DirectoryChooserActivity activity;
-	private Intent launchIntent;
+    private DirectoryChooserActivity activity;
+    private Intent launchIntent;
 
-	@Before
-	public void setup() {
-		activity = Mockito.mock(DirectoryChooserActivity.class);
-		// Robolectric doesn't support the ICS ActionBar.
-		Mockito.doNothing().when(activity).setupActionBar();
+    @Before
+    public void setup() {
+        activity = Mockito.mock(DirectoryChooserActivity.class);
+        // Robolectric doesn't support the ICS ActionBar.
+        Mockito.doNothing().when(activity).setupActionBar();
 
-		launchIntent = new Intent();
-		Mockito.doReturn(launchIntent).when(activity).getIntent();
-	}
+        launchIntent = new Intent();
+        Mockito.doReturn(launchIntent).when(activity).getIntent();
+    }
 
-	@Test
-	public void testSmokeInit() {
-		activity.onCreate(null);
-	}
+    @Test
+    public void testSmokeInit() {
+        activity.onCreate(null);
+    }
 
-	@Test
-	public void testSmokeInitWithExtras() {
-		launchIntent.putExtra(DirectoryChooserActivity.EXTRA_NEW_DIR_NAME,
-				"my dir");
-		activity.onCreate(null);
-	}
+    @Test
+    public void testSmokeInitWithExtras() {
+        launchIntent.putExtra(DirectoryChooserActivity.EXTRA_NEW_DIR_NAME,
+                "my dir");
+        activity.onCreate(null);
+    }
 }
